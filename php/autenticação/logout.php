5b64fd4f4072e94d1d2c008e0e17c6113055af6f
Luigi2507 <luigif2507@gmail.com>
2025-11-25 20:43:45 -0300

    Update logout.php

--- php/autenticação/logout.php
+++ php/autenticação/logout.php
@@ -1,1 +1,20 @@
+<?php
+session_start();
 
+//Caso exista sessão, destrói
+if (isset($_SESSION['user_id'])) {
+    session_unset();
+    session_destroy();
+
+    echo json_encode([
+        "status" => "success",
+        "message" => "Logout realizado com sucesso."
+    ]);
+    exit;
+}
+
+echo json_encode([
+    "status" => "error",
+    "message" => "Nenhum usuário logado."
+]);
+?>
