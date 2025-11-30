538c0b820a6a2c3b29e0a871e2990c506dd01361
Luigi2507 <luigif2507@gmail.com>
2025-11-30 20:51:13 -0300

    Update createdb.php

--- php/createdb.php
+++ php/createdb.php
@@ -26,9 +26,9 @@
     // Tabela: usuarios
     $sql = "CREATE TABLE IF NOT EXISTS users (
         id INT AUTO_INCREMENT PRIMARY KEY,
-        username VARCHAR(100) NOT NULL,
+        name VARCHAR(100) NOT NULL,
         email VARCHAR(120) NOT NULL UNIQUE,
-        user_password VARCHAR(150) NOT NULL
+        password VARCHAR(150) NOT NULL
     )";
 
     if (mysqli_query($conn, $sql)) {
